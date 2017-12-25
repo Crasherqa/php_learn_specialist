780a5c2bda0c9f8217d9da1b31b14bc3e1532849
Crasherqa <procrasher@gmail.com>
2017-12-25 17:47:56 +0200

    Iclude all in one file. Begin level 2

--- index.php
+++ index.php
@@ -8,13 +8,39 @@ require_once "inc/data.inc.php";
     $mon = strftime('%B');
     $year = strftime('%Y');
 
+    //Ініціалізація заголовка сторінки
+    $title = 'Сайт нашей школы';
+    $header = "$welcome, Гость!";
+    $id = strtolower(strip_tags($_GET['id']));
+    switch ($id) {
+      case 'about':
+        $title = 'О сайте';
+        $header = 'О нашем сайте';
+        break;
+
+      case 'contact':
+        $title = 'Контакты';
+        $header = 'Обратная связь';
+        break;
+      
+      case 'table':
+        $title = 'Таблица умножения';
+        $header = 'Таблица умножения';
+        break;
+
+      case 'calc':
+        $title = 'Он-лайн калькулятор';
+        $header = 'Калькулятор';
+        break;
+    }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-  <title>Сайт нашей школы</title>
+  <title><?php echo $title?></title>
   <meta charset="utf-8" />
   <link rel="stylesheet" href="style.css" />
 </head>
@@ -31,11 +57,27 @@ require_once "inc/data.inc.php";
 
   <div id="content">
     <!-- Заголовок -->
-    <h1><?= $welcome ?></h1>
+    <h1><?php echo $header?></h1>
     <!-- Заголовок -->
     <!-- Область основного контента -->
-      <?
-      require_once "inc/index.inc.php";
+      <?php
+      switch ($id) {
+        case 'about':
+          include 'about.php';
+          break;
+        case 'contact':
+          include 'contact.php';
+          break;
+        case 'table':
+          include 'table.php';
+          break;
+        case 'calc':
+          include 'calc.php';
+          break;
+        
+        default:
+          include 'index.inc.php';
+      }
       ?>
     <!-- Область основного контента -->
   </div>
